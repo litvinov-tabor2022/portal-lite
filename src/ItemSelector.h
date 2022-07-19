fd0c2730f9ac3a3630784e1b7e58cc4351b882b5
Jenda Kolena <jendakolena@gmail.com>
2022-07-19 09:42:50 +0200

    Support up to +10 points

--- src/ItemSelector.h
+++ src/ItemSelector.h
@@ -27,12 +27,19 @@ public:
 
 private:
     int curr = 0; // the overflow must be allowed
-    static const u8 max = 3;
+    static const u8 max = 10;
 
     SelectedOp ops[max] = {
             SelectedOp{.name="+1", .bonusPointsDelta=1},
             SelectedOp{.name="+2", .bonusPointsDelta=2},
             SelectedOp{.name="+3", .bonusPointsDelta=3},
+            SelectedOp{.name="+4", .bonusPointsDelta=4},
+            SelectedOp{.name="+5", .bonusPointsDelta=5},
+            SelectedOp{.name="+6", .bonusPointsDelta=6},
+            SelectedOp{.name="+7", .bonusPointsDelta=7},
+            SelectedOp{.name="+8", .bonusPointsDelta=8},
+            SelectedOp{.name="+9", .bonusPointsDelta=9},
+            SelectedOp{.name="+10", .bonusPointsDelta=10},
     };
 };
 
